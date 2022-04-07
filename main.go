ae48a2bf37a01f4baae99658be217a48347d6db0
ChaosNyaruko <cabbageparadise@gmail.com>
2022-04-07 21:45:48 +0800

    bubble and select sort completed by copilot

--- main.go
+++ main.go
@@ -6,6 +6,30 @@ type I interface {
 	Go() string
 }
 
+// bubble sort
+func bubble(nums []int) {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := 0; j < len(nums)-i-1; j++ {
+			if nums[j] > nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+			}
+		}
+	}
+}
+
+// select sort nums using select sort algorithm
+func selectSort(nums []int) {
+	for i := 0; i < len(nums)-1; i++ {
+		min := i
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] < nums[min] {
+				min = j
+			}
+		}
+		nums[i], nums[min] = nums[min], nums[i]
+	}
+}
+
 func main() {
 	mi := my{}
 	fmt.Println(mi.Go())
