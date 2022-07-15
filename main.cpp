9e0d35ac57da91daa38dfe6d81893f37572d600b
ChaosNyaruko <cabbageparadise@gmail.com>
2022-07-15 21:30:54 +0800

    some const params tests for cpp
    test descriptions?

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include "stdc++.h"
 
 int foo();
+int add(int index) {
+    index = index + 1;
+    return index;
+}
+
 int main() {
     std::vector<int> res = {};
     for (auto x:res) {
         std::cout << x << std::endl;
     }
     std::cout << "hello world" << std::endl;
-    printf("%d", foo());
+    printf("%d\n", foo());
+    printf("%d\n", add(1));
     return 0;
 }
 
