b7471ebc8e4a4c41a5b4e018a406bd067523d188
ChaosNyaruko <cabbageparadise@gmail.com>
2022-08-06 16:33:06 +0800

    fmt vs log Print

--- simplego/stdouterr.go
+++ simplego/stdouterr.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 var stdout = log.New(os.Stdout, "info:", log.LstdFlags)
 var stderr = log.New(os.Stderr, "error:", log.LstdFlags)
 
 func main() {
-	stdout.Println("hello")
-	stderr.Println("bye")
+	fmt.Print("no auto lf")
+	log.Print("auto lf")
+	fmt.Print("no auto lf")
+	for {
+		time.Sleep(1 * time.Second)
+		// stdout.Print("\033[31mhello\n\033[0m")
+		// stdout.Print("\033[31mhello\033[0m\n")
+		fmt.Print("\033[31mhello\n\033[0m")
+	}
 }
