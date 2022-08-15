a975364bbace602765e8310f2dd20edbc50eb145
ChaosNyaruko <cabbageparadise@gmail.com>
2022-08-15 21:56:27 +0800

    a simple cpp compile_commands demo: ~ is a fake, using real path instead

--- main.cpp
+++ main.cpp
@@ -2,22 +2,19 @@
 
 int foo();
 int add(int index) {
-    index = index + 1;
-    return index;
+  index = index + 1;
+  return index;
 }
 
 int main() {
-    std::vector<int> res = {};
-    for (auto x:res) {
-        std::cout << x << std::endl;
-    }
-    std::cout << "hello world" << std::endl;
-    printf("%d\n", foo());
-    printf("%d\n", add(1));
-    return 0;
+  std::vector<int> res = {};
+  for (auto x : res) {
+    std::cout << x << std::endl;
+  }
+  std::cout << "hello world" << std::endl;
+  printf("%d\n", foo());
+  printf("%d\n", add(1));
+  return 0;
 }
 
-int foo() {
-    return 20;
-}
-
+int foo() { return 20; }
