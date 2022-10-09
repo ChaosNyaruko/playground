1f261bf2ac6c20dadaf814d98541b462d97a4320
ChaosNyaruko <cabbageparadise@gmail.com>
2022-10-09 17:10:32 +0800

    finish hw10

--- cs61a/hw10/hw10.scm
+++ cs61a/hw10/hw10.scm
@@ -1,14 +1,25 @@
 (define (accumulate combiner start n term)
   (if (= n 0)
       start
-      'YOUR-CODE-HERE
+      (combiner (term n) (accumulate combiner start (- n 1) term))
   )
 )
 
 (define (accumulate-tail combiner start n term)
-  'YOUR-CODE-HERE
+  (define (helper combiner start n term)
+    (if (= n 0)
+      start
+      (helper combiner (combiner start (term n)) (- n 1) term)
+      )
+    )
+  (helper combiner start n term)
 )
 
+; (define-macro (list-of expr for var in seq if filter-fn)
+;               (list 'map (list 'lambda (list var) expr) (list 'filter (list 'lambda (list var) filter-fn) seq))
+; )
+
+; using quasi-quote
 (define-macro (list-of expr for var in seq if filter-fn)
-  'YOUR-CODE-HERE
+              `(map (lambda (,var) ,expr) (filter (lambda (,var) ,filter-fn) ,seq))
 )
