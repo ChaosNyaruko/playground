821e35cdf3564587ef38acd73e15764b107eafa2
ChaosNyaruko <cabbageparadise@gmail.com>
2024-05-23 21:15:42 +0800

    894.cpp

--- debug.hpp
+++ debug.hpp
@@ -11,3 +11,25 @@ inline void print(std ::vector<std::string> &&vec) {
     printf("%s\n", v.c_str());
   }
 }
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+void print(TreeNode* node) {
+  if (node == nullptr) {
+    printf("null,");
+    return;
+  }
+  int val = node->val;
+  printf("%d,", val);
+  print(node->left);
+  print(node->right);
+  return;
+}
