a0f74036beb45531b2c429896ed1c4072e1e2ded
ChaosNyaruko <cabbageparadise@gmail.com>
2024-12-20 14:47:09 +0800

    sqlite3 (embeded) is amazing

--- simplego/sqlite.go
+++ simplego/sqlite.go
@@ -3,15 +3,63 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/ncruces/go-sqlite3/driver
+	"log"
+
+	_ "github.com/ncruces/go-sqlite3/driver"
+	_ "github.com/ncruces/go-sqlite3/embed"
 )
 
 var db *sql.DB
 
+type Album struct {
+	// ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
+// addAlbum adds the specified album to the database,
+// returning the album ID of the new entry
+func addAlbum(alb Album) (int64, error) {
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	return id, nil
+}
+
+// albumsByArtist queries for albums that have the specified artist name.
+func albumsByArtist(name string) ([]Album, error) {
+	// An albums slice to hold data from returned rows.
+	var albums []Album
+
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
+}
+
 func main() {
 	// Get a database handle.
 	var err error
-	db, err = sql.Open("sqlite", )
+	db, err = sql.Open("sqlite3", "file:playing.db")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,4 +69,24 @@ func main() {
 		log.Fatal(pingErr)
 	}
 	fmt.Println("Connected!")
+	var version string
+	err = db.QueryRow(`SELECT sqlite_version()`).Scan(&version)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("version: ", version)
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+	albID, err := addAlbum(Album{
+		Title:  "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price:  49.99,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID of added album: %v\n", albID)
 }
